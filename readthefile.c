576fff5ebcc9bb71c66c2457cf5d0222ecfdf8b2
AngeKT <dotou2021@gmail.com>
2023-06-18 20:43:14 +0100

    I've solved all the issues with this one

--- readthefile.c
+++ readthefile.c
@@ -2,85 +2,87 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct{
-	char name[21];
+struct Contributor{
+	char* name;
     	int level;
 	struct Contributor* next;
-} Contributor;
+};
 
-typedef struct{
-	char skill_name[21];
-    	Contributor* ctbrNames;
+struct Skills{
+	char* skill_name;
+    	struct Contributor* ctbrNames;
     	struct Skills* next;
-} Skills;
+};
 
-typedef struct{
+struct Roles{
 	int level;
 	char name[21];
-} Roles;
+};
 
-typedef struct{
+struct Projects{
     int nbr_of_day;
     int score;
     int best_before;
     int nbr_roles;
-    Roles_t* roles;
+    struct Roles* roles;
     char name[21];
-} Projects;
+};
 
 
 //This function will help us each time we find a new skill to add to the linked list. 
-void add_skills(Skills** head_add, char* skillName){
-	Skills* head = *head_add;
-    	Skills* newNode = malloc(sizeof(Skills));
-	newNode->skill_name = skillName;
-	newNode->ctbrNames = NULL;
-	newNode->next = NULL;
-	
-	if (head == NULL){
-		*head = newNode;
-		return;
-	}
-	
-	else{	//We will loop till we find the last element of the list
-		Skills parcours = **head_add;
-		while (parcours.next != NULL)
-			parcours = *(parcours.next);
-		parcours = newNode;
-	}
+void add_skills(struct Skills** head_add, char* skillName) {
+    struct Skills* head = *head_add;
+    struct Skills* newNode = malloc(sizeof(struct Skills));
+    strncpy(newNode->skill_name, skillName, sizeof(newNode->skill_name) - 1); 
+    newNode->ctbrNames = NULL;
+    newNode->next = NULL;
+
+    if (head == NULL) {
+        *head_add = newNode;  
+        return;
+    } else {
+        // Find the last element of the list
+        struct Skills* parcours = head;
+        while (parcours->next != NULL) {
+            parcours = parcours->next;
+        }
+        parcours->next = newNode;  
+    }
 }
+
 
 //This function will help us each time we find a new contributor for a skill to add it to the linked list for this skill.
-void add_contributors(Contributor** head_add, char* name, int niveau){
-	Contributor* head = *head_add;
-    	Contributor* newNode = malloc(sizeof(Contributor));
-	newNode->name =	name;
-	newNode->level = niveau;
-	newNode->next = NULL;
-	
-	if (head == NULL){
-		*head = newNode;
-		return;
-	}
-	
-	else{
-		Contributor parcours = **head_add;
-		while (parcours.next != NULL)
-			parcours = *(parcours.next);
-		parcours = newNode;
-	}
+void add_contributors(struct Contributor** head_add, char* name, int niveau) {
+    struct Contributor* head = *head_add;
+    struct Contributor* newNode = malloc(sizeof(struct Contributor));
+    strncpy(newNode->name, name, sizeof(newNode->name) - 1);
+    newNode->level = niveau;
+    newNode->next = NULL;
+
+    if (head == NULL){
+        *head_add = newNode;
+        return;
+    } 
+    else{
+        struct Contributor* parcours = *head_add;
+        while (parcours->next != NULL) {
+            parcours = parcours->next;
+    	}
+        parcours->next = newNode;
+    }
 }
 
 
 
-int searchSkill(Skills* head, char* skillName){
-	Skills* parcours = head;
+
+int searchSkill(struct Skills* head, char* skillName){
+	struct Skills* parcours = head;
 	int count = -1;//This variable is for knowing the position of the skill if we happen to find it
 	do{
 		if (strcmp(parcours->skill_name, skillName) == 0){
 			count++;
-			free(ptr);
-    			ptr = NULL;
+			free(parcours);
+    			parcours = NULL;
     			return count;
     		}
 		else{
@@ -107,10 +109,11 @@ int main(int argc, char *argv[]){
         return 1;
     }//This is to handle a potential error when our programme was given no or empty file.
 
-    int nbrContributors, int nbrProjects;
+    int nbrContributors;
+    int nbrProjects;
     fscanf(file, "%d %d", &nbrContributors, &nbrProjects);
 
-    Skills* ptr_skills = NULL; //Here we define a pointer of the structure Skills initialized at NULL to use it after each reading.
+    struct Skills* ptr_skills = NULL; //Here we define a pointer of the structure Skills initialized at NULL to use it after each reading.
     
     for (int i = 0; i < nbrContributors; i++){
     	char currentContributor[21]; 
@@ -131,8 +134,8 @@ int main(int argc, char *argv[]){
    		}
    		
    		else{//Here the skill has already be created. So, we will just have to add the contributor.
-   			int position = searchSkill(ptr_skills, skillName) 
-   			Skills* parcours = ptr_skills;
+   			int position = searchSkill(ptr_skills, skillName); 
+   			struct Skills* parcours = ptr_skills;
    			for (int loop = 0; loop < position; loop++){
    				parcours = parcours->next;
    			add_contributors(&(parcours->ctbrNames), currentContributor, niveau);
@@ -142,24 +145,23 @@ int main(int argc, char *argv[]){
    }
 
 //Once we've finished with each contributor and its skills, let's take care of the project now.  
-    
-    
-    
-    for (int i = 0; i < nbrProjects; i++){
-        fscanf(file, "%s", &projects[i].name);
-        fscanf(file, "%d", &projects[i].nbr_of_day);
-        fscanf(file, "%d", &projects[i].score);
-        fscanf(file, "%d", &projects[i].best_before);
-        fscanf(file, "%d", &projects[i].nbr_roles);
+	struct Projects* projects = (struct Projects*) malloc(nbrProjects * sizeof(struct Projects));
+	for (int i = 0; i < nbrProjects; i++){
+		fscanf(file, "%s", projects[i].name);
+		fscanf(file, "%d", &projects[i].nbr_of_day);
+		fscanf(file, "%d", &projects[i].score);
+		fscanf(file, "%d", &projects[i].best_before);
+		fscanf(file, "%d", &projects[i].nbr_roles);
 
-      	projects[i].roles = (Roles_t*) malloc(current_project->nbr_roles * sizeof(Roles_t));
-        for (int j = 0; j < projects[i].nbr_roles; j++) {
-            fscanf(file, "%s", &roles[i].name);
-            fscanf(file, "%d", &roles[i].level);
-            fgetc(file);
-        }
-    }
+	      	projects[i].roles = (struct Roles*) malloc(projects[i].nbr_roles * sizeof(struct Roles));
+		for (int j = 0; j < projects[i].nbr_roles; j++) {
+		    fscanf(file, "%s", projects[i].roles[i].name);
+		    fscanf(file, "%d", &projects[i].roles[i].level);
+		    fgetc(file);
+		}
+	    }
 
     fclose(file);
+    
     return 0;
 }
